9e6834b8247e285b533e81347c48028c8a6fa82d
jcadam14 <41971533+jcadam14@users.noreply.github.com>
2024-01-12 12:42:35 -0700

    Added lines to the init.sql to create filing api database (#97)
    This includes creating the filing_user role, which will own
    the filing database.  Also creates the filing schema, and
    sets the filing_user search to filing.

--- dev_setup/db/init.sql
+++ dev_setup/db/init.sql
@@ -6,8 +6,16 @@ CREATE DATABASE financial_institutions;
 CREATE USER fi WITH PASSWORD 'fi';
 GRANT ALL PRIVILEGES ON DATABASE financial_institutions TO fi;
 
+CREATE USER filing_user WITH PASSWORD 'filing_user';
+CREATE DATABASE filing OWNER filing_user;
+
 \c keycloak
 GRANT ALL ON SCHEMA public TO keycloak;
 
 \c financial_institutions
 GRANT ALL ON SCHEMA public TO fi;
+
+\c filing
+CREATE SCHEMA filing;
+ALTER SCHEMA filing OWNER TO filing_user;
+ALTER ROLE filing_user IN DATABASE filing SET search_path TO filing;
