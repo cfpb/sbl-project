f51fd63a46f7065b310027ce1bb366212a7c5aed
Adam <41971533+jcadam14@users.noreply.github.com>
2024-03-11 14:50:34 -0400

    Updated script to find pg container name instead of having it hardcoded

--- dev_setup/mock_data/insert_filing_period.sh
+++ dev_setup/mock_data/insert_filing_period.sh
@@ -1,2 +1,4 @@
-docker cp ./filing_period_insert.sql sbl-project-pg-1:/filing_period_insert.sql
-docker exec sbl-project-pg-1 psql -U filing_user filing -f /filing_period_insert.sql
+export pg_container=$(docker ps --format '{{.Names}}' | grep pg)
+echo $pg_container
+docker cp ./filing_period_insert.sql $pg_container:/filing_period_insert.sql
+docker exec $pg_container psql -U filing_user filing -f /filing_period_insert.sql
