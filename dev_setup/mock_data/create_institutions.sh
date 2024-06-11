218e81b541332b03b9865ca74a2eafa1c8fdd37e
jcadam14 <41971533+jcadam14@users.noreply.github.com>
2024-06-11 15:03:41 -0600

    Moved institution json to sbl-test-data and updated shell script (#224)

--- dev_setup/mock_data/create_institutions.sh
+++ dev_setup/mock_data/create_institutions.sh
@@ -10,16 +10,17 @@ export RT_ACCESS_TOKEN=$(curl 'localhost:8880/realms/regtech/protocol/openid-con
 
 curl localhost:8881/v1/admin/me -H "Authorization: Bearer ${RT_ACCESS_TOKEN}" | jq -r '.'
 
-inst_json_files=( 'test_bank_123.json' 'test_bank_456.json' 'test_sub_bank_456.json' )
+inst_json_files=( $(curl -v https://api.github.com/repos/cfpb/sbl-test-data/contents/financial_institutions\?ref\=5-add-test-financial-institution-jsons-to-repo | jq -r '.[].download_url') )
 
 for file in "${inst_json_files[@]}"
 do
-  lei=$(cat $file | jq -r '.lei')
+  json=$(curl $file | jq '.')
+  lei=$(echo $json | jq -r '.lei')
   domain=$(echo $lei | tr '[:upper:]' '[:lower:]').local
   curl localhost:8881/v1/institutions/ -X POST \
       -H "Authorization: Bearer ${RT_ACCESS_TOKEN}" \
       -H 'Content-Type: application/json' \
-      --data-binary "@$file" | jq -r '.'
+      -d "$json" | jq -r '.'
   curl "localhost:8881/v1/institutions/$lei/domains" -X POST \
       -H "Authorization: Bearer ${RT_ACCESS_TOKEN}" \
       -H 'Content-Type: application/json' \
