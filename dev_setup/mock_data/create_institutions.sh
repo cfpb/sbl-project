dee6c86b5bebc1a9afa05f4264872a9108dfa976
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2024-06-24 11:49:25 -0400

    feat: update institution creation script

--- dev_setup/mock_data/create_institutions.sh
+++ dev_setup/mock_data/create_institutions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export RT_ACCESS_TOKEN=$(curl 'localhost:8880/realms/regtech/protocol/openid-connect/token' \
+auth_base='http://localhost:8880'
+user_fi_base='http://localhost:8881'
+
+export RT_ACCESS_TOKEN=$(curl "${auth_base}/realms/regtech/protocol/openid-connect/token" \
 -X POST \
 -H 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'username=admin1' \
@@ -8,7 +11,7 @@ export RT_ACCESS_TOKEN=$(curl 'localhost:8880/realms/regtech/protocol/openid-con
 --data-urlencode 'grant_type=password' \
 --data-urlencode 'client_id=regtech-client' | jq -r '.access_token')
 
-curl localhost:8881/v1/admin/me -H "Authorization: Bearer ${RT_ACCESS_TOKEN}" | jq -r '.'
+curl $user_fi_base/v1/admin/me -H "Authorization: Bearer ${RT_ACCESS_TOKEN}" | jq -r '.'
 
 inst_json_files=( 'test_bank_123.json' 'test_bank_456.json' 'test_sub_bank_456.json' )
 
@@ -16,12 +19,12 @@ for file in "${inst_json_files[@]}"
 do
   lei=$(cat $file | jq -r '.lei')
   domain=$(echo $lei | tr '[:upper:]' '[:lower:]').local
-  curl localhost:8881/v1/institutions/ -X POST \
+  curl $user_fi_base/v1/institutions/ -X POST \
       -H "Authorization: Bearer ${RT_ACCESS_TOKEN}" \
       -H 'Content-Type: application/json' \
       --data-binary "@$file" | jq -r '.'
-  curl "localhost:8881/v1/institutions/$lei/domains" -X POST \
+  curl $user_fi_base/v1/institutions/$lei/domains -X POST \
       -H "Authorization: Bearer ${RT_ACCESS_TOKEN}" \
       -H 'Content-Type: application/json' \
-      -d "[{\"domain\": \"$domain\"}]" | jq -r '.'
+      -d "[{\"domain\": \"$domain\"},{\"domain\": \"cfpb.gov\"}]" | jq -r '.'
 done
