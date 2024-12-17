8c438c17c3b063ceefdc131b79cbf1b925a261e5
Adam <41971533+jcadam14@users.noreply.github.com>
2024-12-16 17:27:38 -0700

    Changed the check url to be /health/ready

--- dev_setup/keycloak/healthcheck.sh
+++ dev_setup/keycloak/healthcheck.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # this is used because keycloak's image does not have curl or wget commands
-exec 3<>/dev/tcp/localhost/8080 || { echo "Connection Failed"; exit 1; }
+exec 3<>/dev/tcp/localhost/9000 || { echo "Connection Failed"; exit 1; }
 
-echo -e "GET /realms/regtech/protocol/openid-connect/certs HTTP/1.1\r\nHost: localhost\r\n\r\n" >&3
+echo -e "GET /health/ready HTTP/1.1\r\nHost: localhost\r\n\r\n" >&3
 
 response=$(head -n 1 <&3)
 
